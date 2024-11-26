9c67bb6a06d92cbc55334bf39706097f2dce30eb
evldmt <lvadim3110@gmail.com>
2024-11-26 13:51:56 +0900

    fasf

---
+++ Project ise/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Project ise
+//
+//  Created by Vadim Lifrov on 11/26/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+sfqafqw
+qwfqfqfw
+wfqfqwf
+
